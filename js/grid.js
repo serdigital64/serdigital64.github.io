cd4a2c9e6c8420e4b5374254d78f5521374ae014
SerDigital64 <serdigital64@gmail.com>
2021-10-08 09:03:04 -0300

    Publish 8-Oct-2021

---
+++ js/grid.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){var e;(e=$(".dream-grid").masonry({itemSelector:".dream-column"})).imagesLoaded().progress(function(){return e.masonry("layout")}),window.hasTwitterEmbed&&window.twttr.ready(function(r){return r.events.bind("loaded",function(){return e.masonry("layout")})})});
