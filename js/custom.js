4162ab355aa636be1ab337d023cee0362e67f75b
SerDigital64 <serdigital64@gmail.com>
2021-12-31 18:45:52 -0300

    Promote

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+window.background = {{ .Site.Params.background }}
+window.defaultDark = {{ .Site.Params.defaultDark }}
+window.backgroundDark = {{ .Site.Params.backgroundDark }}
+window.backgroundImageDark = {{ .Site.Params.backgroundImageDark }}
+window.darkNav = {{ .Site.Params.darkNav }}
+window.maxTags = {{ .Site.Params.maxTags }}
+window.hasTwitterEmbed = {{ .Site.Params.hasTwitterEmbed }}
+window.fixedNav = {{ .Site.Params.fixedNav }}
